28c8bcedb536f0f8aff951524b6035009d6fd7f8
Nat Mote <nat@natmote.net>
2021-04-20 12:49:52 -0700

    Add React

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+render(
+  React.createElement('h1', null, "Hello World from Parcel + React"),
+  document.getElementById('root')
+);

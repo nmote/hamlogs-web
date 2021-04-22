bd22b9bbc498c16c5b3b5f607759ea67267ece00
Nat Mote <nat@natmote.net>
2021-04-22 12:08:07 -0700

    First cut at a working site using the hamlogs npm package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,111 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {CSVToAdif} from 'hamlogs';
+
+class Root extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {outputText: null};
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(callsign, park, csvText) {
+    // TODO handle error
+    const adif = CSVToAdif(callsign, park, csvText).value;
+    this.setState({outputText: adif});
+  }
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.handleSubmit}/>
+        <OutputArea text={this.state.outputText}/>
+      </div>
+    );
+  }
+}
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      callsign: '',
+      park: '',
+      file: null,
+    };
+
+    this.handleCallsignChange = this.handleCallsignChange.bind(this);
+    this.handleParkChange = this.handleParkChange.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleFileChange(event) {
+    this.setState({file: event.target.files[0]});
+  }
+
+  handleCallsignChange(event) {
+    this.setState({callsign: event.target.value});
+  }
+
+  handleParkChange(event) {
+    this.setState({park: event.target.value});
+  }
+
+  handleSubmit(event) {
+    const reader = new FileReader();
+
+    const callsign = this.state.callsign;
+    const park = this.state.park;
+
+    reader.onload = (evt) => {
+      this.props.onSubmit(callsign, park, evt.target.result);
+    };
+    reader.readAsText(this.state.file);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Your callsign:
+          <input type="text" value={this.state.callsign} onChange={this.handleCallsignChange} />
+        </label>
+        <br/>
+        <label>
+          Your park:
+          <input type="text" value={this.state.park} onChange={this.handleParkChange} />
+        </label>
+        <br/>
+        <label>
+          CSV File (<a href="https://raw.githubusercontent.com/nmote/hamlogs/main/fixtures/K-3213.csv">example</a>):
+          <input type="file" onChange={this.handleFileChange} />
+        </label>
+        <br/>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+class OutputArea extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    if (this.props.text == null) {
+      return null;
+    }
+
+    return <textarea value={this.props.text} disabled={true} />
+  }
+}
 
 render(
-  <h1>Hello World from Parcel + React + JSX</h1>,
+  <Root/>,
   document.getElementById('root')
 );
